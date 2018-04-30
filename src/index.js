73cf4ea4604196a16965a9de3e0c8a5fd1f5d04e
ndrewr <uncle.optimus@gmail.com>
2018-04-30 00:03:57 -0700

    Initial commit; Basic schema and server setup files

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+let links = [{
+	id: 'link-0',
+	url: 'www.howtographql.com',
+	description: 'Fullstack tutorial for GraphQL',
+}]
+
+let idCount = links.length
+
+const resolvers = {
+	Query: {
+		info: () => null,
+		feed: () => links,	
+	},
+	Mutation: {
+		post: (root, args) => {
+			const link = {
+				id: `link-${idCount++}`,
+				description: args.description,
+				url: args.url,
+			}
+			links.push(link)
+			return link
+		}
+	},
+/* below resolvers are so trivial they can be omitted
+	Link: {
+		id: (root) => root.id,
+		description: (root) => root.description,
+		url: (root) => root.url,
+	}
+*/
+}
+
+const server = new GraphQLServer({
+	typeDefs: './src/schema.graphql',
+	resolvers,
+})
+
+server.start(() => console.log(`Server is running on http://localhost:4000`))
+
